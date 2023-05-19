1f9f34638658d7d5b2a189a5e083f7e8490a406f
JuanPorta <juanporta.c@gmail.com>
2023-05-19 18:11:30 -0300

    ahora si, este commit es la union de los dos menus con el inicio

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+function toggleDesktopMenu(){
+    desktopMenu.classList.toggle("inactive");
+    console.log("click");
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
+    console.log("click");
+}
+const email = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const logo = document.querySelector(".menu");
 
+email.addEventListener("click", toggleDesktopMenu);
+logo.addEventListener("click", toggleMobileMenu);
