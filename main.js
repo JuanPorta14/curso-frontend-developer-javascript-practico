986cea210b133a0cfe2621aae315fd57007b3375
JuanPorta <juanporta.c@gmail.com>
2023-05-21 03:38:39 -0300

    automatizacion de la creacion de productos en la tienda, entre otras cosas

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-function toggleDesktopMenu(){
-    desktopMenu.classList.toggle("inactive");
-    console.log("click");
-}
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle("inactive");
-    console.log("click");
-}
 const email = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const logo = document.querySelector(".menu");
+const compras = document.querySelector(".product-detail");
+const carritoImg = document.querySelector(".navbar-shopping-cart");
 
-email.addEventListener("click", toggleDesktopMenu);
-logo.addEventListener("click", toggleMobileMenu);
+
+
+email.addEventListener("click", () => {
+    desktopMenu.classList.toggle("inactive")
+    if (compras.classList.contains("inactive") == false){
+        compras.classList.toggle("inactive");
+    }
+    
+
+});
+logo.addEventListener("click", () => {
+    mobileMenu.classList.toggle("inactive")
+    if (compras.classList.contains("inactive") == false){
+        compras.classList.toggle("inactive");
+    }
+});
+carritoImg.addEventListener("click", () =>{
+    compras.classList.toggle("inactive")
+    if (desktopMenu.classList.contains("inactive") == false){
+        desktopMenu.classList.toggle("inactive");
+    }
+    else if (mobileMenu.classList.contains("inactive") == false){
+        mobileMenu.classList.toggle("inactive");
+    }
+
+});
+
